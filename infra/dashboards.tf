c0ddbab2f1355c016ccc1d48a501bc275504e6a1
attichouse <77102070+attichouse@users.noreply.github.com>
2022-12-07 14:34:40 +0100

    fixed correct mail and id

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "period": 300,
         "stat": "Maximum",
         "region": "eu-west-1",
-        "title": "Total number of accounts"
+        "title": "Total number of carts"
       }
     }
   ]
