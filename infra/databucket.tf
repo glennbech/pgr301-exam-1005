97fb77f20e8d956885686fb4c9ac69e70082e6ac
attichouse <77102070+attichouse@users.noreply.github.com>
2022-12-07 15:37:48 +0100

    trying to create s3 bucket

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -1,6 +1,6 @@
 # Jim; this just fails ... commented it out ! We need to figure this out later, starting new task instead...
 
 variable "aws_s3_bucket" {
-    bucket = "analytics-1005"
-    region = "eu-west-1"
+    default = "analytics-1005"
+    type = string
 }
